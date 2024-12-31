ca52a9168f09677c5c95dbb59df67524267d4d73
Harrison Turpin <harrison.bradley.turpin@gmail.com>
2024-12-31 19:54:29 +0000

    Initial commit

---
+++ CfgPatches.hpp
@@ -0,0 +1,14 @@
+class cfgPatches
+{
+	class TAW_AUX_CUSTOMS
+	{
+		name="TAW Aux Customs";
+		author="TAW";
+		requiredAddons[]={};
+		requiredVersion=1.0;
+		weapons[]={};
+		units[]={};
+		magazines[]={};
+		ammo[]={};
+	};
+};
