c7073838705218308c66e552b1f51b4cbf0a0f0a
Harrison Turpin <harrison.bradley.turpin@gmail.com>
2025-03-11 21:24:04 +0000

    Adjust Functions Class

--- CfgFunctions.hpp
+++ CfgFunctions.hpp
@@ -1,7 +1,7 @@
 class CfgFunctions {
 	class TAW {
 		tag = "TAW";
-		class TAW_AUX_CUSTOMS {
+		class CUSTOMS {
 			file="TAW_AUX_CUSTOMS\Functions";
 			//class init{preInit=1;};
 			//class vehicleTextureSwap{}; 
