8456108ee3afb29e0a44d236ce2a18a7527d74b5
Harrison Turpin <harrison.bradley.turpin@gmail.com>
2025-03-19 19:31:53 +0000

    Add M32 & AK-101

--- CfgPatches.hpp
+++ CfgPatches.hpp
@@ -10,9 +10,16 @@ class cfgPatches
 			"A3_Characters_F_Orange",
 			"aceax_gearinfo", //Ace Extended Arsenal Compat
 			"A3_Soft_F_Exp", //Qilin
+			"A3_Weapons_F",	
+			"A3_Weapons_F_Exp",
+			"ace_main",	
+			"rhsusf_main",
+			"rhsusf_weapons",
+			"rhsusf_weapons2",
+			"rhsusf_weapons3"
 		};
 		requiredVersion=1.0;
-		weapons[]={"U_B_FullGhillie_alpine"};
+		weapons[]={"U_B_FullGhillie_alpine", "rhs_weap_m32_sidearm", "arifle_AK101_F", "arifle_AK101_GL_F"};
 		units[]={"B_ghillie_alpine_F", "B_LSV_02_armed_F_custom_recon"};
 		magazines[]={};
 		ammo[]={};
--- CfgWeapons.hpp
+++ CfgWeapons.hpp
@@ -1,10 +1,46 @@
 class cfgWeapons
 {
+	/////////////////////////////
+	//NATO 556 AK-101 Rechamber//
+	/////////////////////////////
+	class arifle_AK12_F;
+	class arifle_AK101_F: arifle_AK12_F {
+		displayName = "AK-101";
+		magazines[] = {};
+		magazineWell[] = {"CBA_556x45_AK"};
+	};
+	class arifle_AK12_GL_F;
+	class arifle_AK101_GL_F: arifle_AK12_GL_F {
+		displayName = "AK-101 (GL)";
+		magazines[] = {};
+		magazineWell[] = {"CBA_556x45_AK","CBA_40mm_M203"};
+	};
+
+
+	////////////////////////////
+	//M32 Sidearm Custom Class//
+	////////////////////////////
+	class rhs_weap_m32_Base_F;
+	class rhs_weap_m32: rhs_weap_m32_Base_F {
+		class WeaponSlotsInfo;
+	};
+	class rhs_weap_m32_sidearm: rhs_weap_m32 {
+		class WeaponSlotsInfo: WeaponSlotsInfo {
+            allowedSlots[] = {701}; // Slots for sidearms
+        };
+		type = 2;
+		handAnim[] = {"OFP2_ManSkeleton","\rhsusf\addons\rhsusf_c_weapons\anims\rhs_hand_M32.rtm"};
+	};
+
+
+
+
+
 	class ItemCore;
 	class UniformItem;
-	///////////////////////////
+	////////////////////////////
 	//Alpine Ghillie Retexture//
-	///////////////////////////
+	////////////////////////////
 	class U_B_FullGhillie_lsh; 
 	class U_B_FullGhillie_alpine: U_B_FullGhillie_lsh
 	{
