78533f7b94ea6e757c1de58d73f6aac25c139620
Harrison Turpin <harrison.bradley.turpin@gmail.com>
2025-03-24 17:44:22 +0000

    Remove AK101 & M32

--- CfgPatches.hpp
+++ CfgPatches.hpp
@@ -9,17 +9,10 @@ class cfgPatches
 			"A3_Characters_F_Exp",
 			"A3_Characters_F_Orange",
 			"aceax_gearinfo", //Ace Extended Arsenal Compat
-			"A3_Soft_F_Exp", //Qilin
-			"A3_Weapons_F",	
-			"A3_Weapons_F_Exp",
-			"ace_main",	
-			"rhsusf_main",
-			"rhsusf_weapons",
-			"rhsusf_weapons2",
-			"rhsusf_weapons3"
+			"A3_Soft_F_Exp" //Qilin
 		};
 		requiredVersion=1.0;
-		weapons[]={"U_B_FullGhillie_alpine", "rhs_weap_m32_sidearm", "arifle_AK101_F", "arifle_AK101_GL_F"};
+		weapons[]={"U_B_FullGhillie_alpine"};
 		units[]={"B_ghillie_alpine_F", "B_LSV_02_armed_F_custom_recon"};
 		magazines[]={};
 		ammo[]={};
