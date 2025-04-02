37c68ca75aaf5f5183708fb3d13fbeeafe7d80c0
Harrison Turpin <harrison.bradley.turpin@gmail.com>
2025-04-02 13:50:31 +0100

    Further Qilin Modifications / Additions

--- CfgVehicles.hpp
+++ CfgVehicles.hpp
@@ -3,11 +3,345 @@ class cfgVehicles
 	class Car;
 	class Car_F: Car {
 		class TextureSources;
+		class Turrets;
 	};
-	//////////////////////////
-	//LSVMk2 (Qilin) CUSTOMS//
-	//////////////////////////
-	class LSV_02_base_F: Car_F{
+	class LSV_01_base_F: Car_F {
+		class CargoTurret;
+		class Turrets: Turrets
+		{
+			class CargoTurret_02: CargoTurret
+			{
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+			class CargoTurret_03: CargoTurret_02
+			{
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+		};
+	};
+	class LSV_01_unarmed_base_F: LSV_01_base_F {
+		class Turrets: Turrets
+		{
+			class CargoTurret_02: CargoTurret_02
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				isPersonTurret=2;
+				forceHideGunner=0;
+				canHideGunner=1;
+				allowLauncherIn=0;
+				allowLauncherOut=1;
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+			class CargoTurret_03: CargoTurret_03
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				isPersonTurret=2;
+				forceHideGunner=0;
+				canHideGunner=1;
+				allowLauncherIn=0;
+				allowLauncherOut=1;
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+			class CargoTurret_01: CargoTurret_02
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+			};
+			class CargoTurret_04: CargoTurret_02
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+			};
+			class CargoTurret_05: CargoTurret_02
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+			};
+			class CargoTurret_06: CargoTurret_01
+			{
+				gunnerInAction="vehicle_passenger_stand_2";
+				forceHideGunner=1;
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+		};
+	};
+	class LSV_01_light_base_F: LSV_01_base_F {
+		class Turrets: Turrets
+		{
+			class CargoTurret_02: CargoTurret_02
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				isPersonTurret=2;
+				forceHideGunner=0;
+				canHideGunner=1;
+				allowLauncherIn=0;
+				allowLauncherOut=1;
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+			class CargoTurret_03: CargoTurret_03
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				isPersonTurret=2;
+				forceHideGunner=0;
+				canHideGunner=1;
+				allowLauncherIn=0;
+				allowLauncherOut=1;
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+			class CargoTurret_01: CargoTurret_02
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+			};
+			class CargoTurret_04: CargoTurret_02
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+			};
+			class CargoTurret_05: CargoTurret_02
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+			};
+			class CargoTurret_06: CargoTurret_01
+			{
+				gunnerInAction="vehicle_passenger_stand_2";
+				forceHideGunner=1;
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+		};
+	};
+	class LSV_02_base_F: Car_F {
 		class TextureSources: TextureSources {
 			class Custom_Black {
 				displayName = "Black (NATO)";
@@ -21,12 +355,263 @@ class cfgVehicles
 				textures[] = {"\TAW_AUX_CUSTOMS\Data\lsv_02\lsv_01_custom_recon_co.paa","\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_02_black_CO.paa","\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_03_black_CO.paa","\A3\Weapons_F_Tank\Launchers\Vorona\Data\Vorona_green_F_CO.paa","\A3\Weapons_F_Tank\Launchers\Vorona\Data\Vorona_green_F_CO.paa"};
 			};
 		};
+		class CargoTurret;
+		class Turrets: Turrets
+		{
+			class CargoTurret_01: CargoTurret
+			{
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+			class CargoTurret_02: CargoTurret_01
+			{
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+			class CargoTurret_03: CargoTurret_01
+			{
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+			class CargoTurret_04: CargoTurret_01
+			{
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+			class CargoTurret_05: CargoTurret_01
+			{
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+		};
+	};
+	class LSV_02_unarmed_base_F: LSV_02_base_F {
+		class Turrets: Turrets
+		{
+			class CargoTurret_01: CargoTurret_01
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				isPersonTurret=2;
+				forceHideGunner=0;
+				canHideGunner=1;
+				allowLauncherIn=0;
+				allowLauncherOut=1;
+			};
+			class CargoTurret_02: CargoTurret_02
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				isPersonTurret=2;
+				forceHideGunner=0;
+				canHideGunner=1;
+				allowLauncherIn=0;
+				allowLauncherOut=1;
+			};
+			class CargoTurret_03: CargoTurret_03
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				isPersonTurret=2;
+				forceHideGunner=0;
+				canHideGunner=1;
+				allowLauncherIn=0;
+				allowLauncherOut=1;
+			};
+			class CargoTurret_04: CargoTurret_04
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				isPersonTurret=2;
+				forceHideGunner=0;
+				canHideGunner=1;
+				allowLauncherIn=0;
+				allowLauncherOut=1;
+			};
+			class CargoTurret_05: CargoTurret_05
+			{
+				gunnerInAction="passenger_inside_7";
+				gunnerAction="passenger_flatground_4_vehicle_passenger_stand_1";
+				isPersonTurret=2;
+				forceHideGunner=0;
+				canHideGunner=1;
+				allowLauncherIn=0;
+				allowLauncherOut=1;
+			};
+			class CargoTurret_06: CargoTurret_01
+			{
+				gunnerInAction="vehicle_passenger_stand_2";
+				forceHideGunner=1;
+				class TurnIn
+				{
+					limitsArrayTop[]=
+					{
+						{55,-105},
+						{55,105}
+					};
+					limitsArrayBottom[]=
+					{
+						{-40,-105},
+						{-30,-40},
+						{-30,40},
+						{-40,105}
+					};
+				};
+				class TurnOut: TurnIn
+				{
+				};
+			};
+		};
 	};
 	class LSV_02_armed_base_F;
+	class O_T_LSV_02_unarmed_F;
 	class O_T_LSV_02_armed_F;
-	//Recon
+	class O_T_LSV_02_AT_F;
+	//////////////////////////
+	//LSVMk2 (Qilin) CUSTOMS//
+	//////////////////////////
+	class B_LSV_02_unarmed_F: O_T_LSV_02_unarmed_F {
+		displayName = "LSV Mk. II (Unarmed)";
+		scope = 2;
+		scopeCurator = 2;
+		side = 1;
+		faction = "BLU_F";
+		crew = "B_Soldier_F";
+		typicalCargo[] = {"B_Soldier_F"};
+
+		hiddenSelectionsTextures[] = {"\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_01_black_CO.paa","\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_02_black_CO.paa","\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_03_black_CO.paa"};
+		textureList[] = {"Custom_Black", 1, "Custom_Recon", 0};
+	};
+	class B_LSV_02_unarmed_F_custom_recon: O_T_LSV_02_unarmed_F {
+		displayName = "LSV Mk. II (Unarmed) (Recon)";
+		scope = 2;
+		scopeCurator = 2;
+		side = 1;
+		faction = "BLU_F";
+		crew = "B_spotter_F";
+		typicalCargo[] = {"B_spotter_F"};
+
+		hiddenSelectionsTextures[] = {"\TAW_AUX_CUSTOMS\Data\lsv_02\lsv_01_custom_recon_co.paa","\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_02_black_CO.paa","\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_03_black_CO.paa"};
+		textureList[] = {"Custom_Black", 0, "Custom_Recon", 1};
+	};
+	class B_LSV_02_armed_F: O_T_LSV_02_armed_F {
+		displayName = "LSV Mk. II (M134)";
+		scope = 2;
+		scopeCurator = 2;
+		side = 1;
+		faction = "BLU_F";
+		crew = "B_Soldier_F";
+		typicalCargo[] = {"B_Soldier_F"};
+
+		hiddenSelectionsTextures[] = {"\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_01_black_CO.paa","\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_02_black_CO.paa","\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_03_black_CO.paa"};
+		textureList[] = {"Custom_Black", 1, "Custom_Recon", 0};
+	};
 	class B_LSV_02_armed_F_custom_recon: O_T_LSV_02_armed_F {
-		displayName = "LSV Mk. II (Recon)";
+		displayName = "LSV Mk. II (M134) (Recon)";
+		scope = 2;
+		scopeCurator = 2;
+		side = 1;
+		faction = "BLU_F";
+		crew = "B_spotter_F";
+		typicalCargo[] = {"B_spotter_F"};
+
+		hiddenSelectionsTextures[] = {"\TAW_AUX_CUSTOMS\Data\lsv_02\lsv_01_custom_recon_co.paa","\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_02_black_CO.paa","\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_03_black_CO.paa"};
+		textureList[] = {"Custom_Black", 0, "Custom_Recon", 1};
+	};
+	class B_LSV_02_AT_F: O_T_LSV_02_AT_F {
+		displayName = "LSV Mk. II (Metis-M)";
+		scope = 2;
+		scopeCurator = 2;
+		side = 1;
+		faction = "BLU_F";
+		crew = "B_Soldier_F";
+		typicalCargo[] = {"B_Soldier_F"};
+
+		hiddenSelectionsTextures[] = {"\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_01_black_CO.paa","\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_02_black_CO.paa","\A3\Soft_F_Exp\LSV_02\Data\CSAT_LSV_03_black_CO.paa"};
+		textureList[] = {"Custom_Black", 1, "Custom_Recon", 0};
+	};
+	class B_LSV_02_AT_F_custom_recon: O_T_LSV_02_AT_F {
+		displayName = "LSV Mk. II (Metis-M) (Recon)";
 		scope = 2;
 		scopeCurator = 2;
 		side = 1;
